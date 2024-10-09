2aedc2f1b634e88c407d1e7996037a97f5181f5d
SVR Kalluru <raghuk.vit@gmail.com>
2024-10-09 08:37:58 +0530

    Adding print statements

--- dispatch.sh
+++ dispatch.sh
@@ -1,29 +1,32 @@
-echo -e "\e[35m Copy Dispatch Service file \e[0m"
+color="\e[35m"
+no_color="\e[0m"
+
+echo -e "$color Copy Dispatch Service file $no_color"
 cp dispatch.service /etc/systemd/system/dispatch.service
 
-echo -e "\e[35m Install GoLang \e[0m"
+echo -e "$color Install GoLang $no_color"
 dnf install golang -y
 
-echo -e "\e[35m Add Application User \e[0m"
+echo -e "$color Add Application User $no_color"
 useradd roboshop
 
-echo -e "\e[35m Create Application Directory \e[0m"
+echo -e "$color Create Application Directory $no_color"
 rm -rf /app
 mkdir /app
 
-echo -e "\e[35m Download Application content \e[0m"
+echo -e "$color Download Application content $no_color"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
 cd /app
 
-echo -e "\e[35m Extract Application Content \e[0m"
+echo -e "$color Extract Application Content $no_color"
 unzip /tmp/dispatch.zip
 
-echo -e "\e[35m Copy Download Application Dependencies \e[0m"
+echo -e "$color Copy Download Application Dependencies $no_color"
 go mod init dispatch
 go get
 go build
 
-echo -e "\e[35m Start Application Service \e[0m"
+echo -e "$color Start Application Service $no_color"
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl restart dispatch
