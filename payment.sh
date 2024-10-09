ac33cba95e580b50cc1a74c7d704015e025b7678
SVR Kalluru <raghuk.vit@gmail.com>
2024-10-09 08:50:43 +0530

    Adding print statements

--- payment.sh
+++ payment.sh
@@ -1,4 +1,5 @@
 source common.sh
+app_name=payment
 
 echo -e "$color Copy Payment Service file $no_color"
 cp payment.service /etc/systemd/system/payment.service
@@ -6,19 +7,7 @@ cp payment.service /etc/systemd/system/payment.service
 echo -e "$color Install Python $no_color"
 dnf install python3 gcc python3-devel -y
 
-echo -e "$color Add Application User $no_color"
-useradd roboshop
-
-echo -e "$color Create Application Directory $no_color"
-rm -rf /app
-mkdir /app
-
-echo -e "$color Download Application Content $no_color"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
-cd /app
-
-echo -e "$color Extract Application Content $no_color"
-unzip /tmp/payment.zip
+app_prerequisites
 
 echo -e "$color Download Application Dependencies $no_color"
 pip3 install -r requirements.txt
