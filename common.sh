4a055bf3370c29dc97e8ac89ef43dcfaf747fa4f
SVR Kalluru <raghuk.vit@gmail.com>
2024-10-10 09:00:00 +0530

    Adding print statements

--- common.sh
+++ common.sh
@@ -115,7 +115,7 @@ maven_setup() {
 
   for sql_file in  schema app-user master-data; do
     print_heading "Load SQL File - $sql_file"
-    mysql -h mysql.rdevopsb81.online -uroot -pRoboShop@1 < /app/db/$sql_file.sql
+    mysql -h mysql.rdevopsb81.online -uroot -pRoboShop@1 < /app/db/$sql_file.sql &>>$log_file
     status_check $?
   done
 
