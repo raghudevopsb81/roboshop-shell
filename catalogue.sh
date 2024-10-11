91a7c3f512dbbd209ddaa76b483674de2a752e2e
Raghu K <raghuk.vit@gmail.com>
2024-10-11 07:55:03 +0530

    Handling Passwords from Input

--- catalogue.sh
+++ catalogue.sh
@@ -2,3 +2,20 @@ source common.sh
 app_name=catalogue
 
 nodejs_setup
+
+print_heading "Copy MongoDB repo file"
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log_file
+status_check $?
+
+print_heading "Install MongoDB Client"
+dnf install mongodb-mongosh -y &>>$log_file
+status_check $?
+
+print_heading "Load Master Data"
+mongosh --host mongodb.rdevopsb81.online </app/db/master-data.js
+status_check $?
+
+print_heading "Restart Catalogue Service"
+systemctl restart catalogue &>>$log_file
+status_check $?
+
--- shipping.sh
+++ shipping.sh
@@ -7,5 +7,21 @@ if [ -z "$1" ]; then
 fi
 
 MYSQL_ROOT_PASSWORD=$1
+
 maven_setup
+
+print_heading "Install MySQL Client"
+dnf install mysql -y   &>>$log_file
+status_check $?
+
+for sql_file in schema app-user master-data; do
+  print_heading "Load SQL File - $sql_file"
+  mysql -h mysql.rdevopsb81.online -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/$sql_file.sql &>>$log_file
+  status_check $?
+done
+
+print_heading "Restart Shipping Service"
+systemctl restart shipping &>>$log_file
+status_check $?
+
 
