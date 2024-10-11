7d45da3136015e54c26d1334c10ff0b282e859e5
Raghu K <raghuk.vit@gmail.com>
2024-10-11 07:42:13 +0530

    Handling Passwords from Input

--- common.sh
+++ common.sh
@@ -46,6 +46,7 @@ systemd_setup() {
 
   print_heading "Copy the Service File"
   cp $scripts_path/$app_name.service /etc/systemd/system/$app_name.service &>>$log_file
+  sed -i -e "s/RABBITMQ_PASSWORD/${RABBITMQ_PASSWORD}/" /etc/systemd/system/$app_name.service &>>$log_file
   status_check $?
 
   print_heading "Start Application Service File"
@@ -115,7 +116,7 @@ maven_setup() {
 
   for sql_file in  schema app-user master-data; do
     print_heading "Load SQL File - $sql_file"
-    mysql -h mysql.rdevopsb81.online -uroot -pRoboShop@1 < /app/db/$sql_file.sql &>>$log_file
+    mysql -h mysql.rdevopsb81.online -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/$sql_file.sql &>>$log_file
     status_check $?
   done
 
--- shipping.sh
+++ shipping.sh
@@ -1,6 +1,11 @@
 source common.sh
 app_name=shipping
 
+if [ -z "$1" ]; then
+  echo INput MySQL Root Password is missing
+  exit 1
+fi
 
+MYSQL_ROOT_PASSWORD=$1
 maven_setup
 
